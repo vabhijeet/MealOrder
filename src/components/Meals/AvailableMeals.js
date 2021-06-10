4e752f8e0c7980fc69d506859581d346284dfef7
Abhijeet Veer <veerabhijeet10@gmail.com>
2021-06-10 10:36:30 +0530

    changed

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -8,11 +8,7 @@ const DummyMeals=[
     {
       id: 'm1',
       name: 'Sushi',
-<<<<<<< HEAD
       description: 'Finest fish and veggies anime lover should try this',
-=======
-      description: 'Finest fish and veggies',
->>>>>>> 5633117d7760823d1ab87535bee56d5cab102343
       price: 22.99,
     },
     {
@@ -24,24 +20,14 @@ const DummyMeals=[
     },
     {
       id: 'm3',
-<<<<<<< HEAD
       name: 'Pizza',
       description: 'Italian, all ones favourite',
-=======
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
->>>>>>> 5633117d7760823d1ab87535bee56d5cab102343
       price: 12.99,
     },
     {
       id: 'm4',
-<<<<<<< HEAD
       name: 'Samosa',
       description: 'delecious Indian snack',
-=======
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
->>>>>>> 5633117d7760823d1ab87535bee56d5cab102343
       price: 18.99,
     },
   ];
